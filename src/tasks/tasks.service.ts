5204348185121da51cc627abf3c95cd2342a95da
Kuldeep Rathod <kuldiprathod375@gmail.com>
2025-03-12 16:47:47 +0530

    complete crud operation

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TasksService {
@@ -10,7 +11,27 @@ export class TasksService {
     return this.tasks;
   }
 
-  createTask(title: string, description: string): Task {
+  getTaskById(id: string): Task | undefined {
+    return this.tasks.find((task) => task.id === id);
+  }
+
+  deleteTask(id: string): Task {
+    const taskIndex = this.tasks.findIndex((task) => task.id === id);
+    const deletedTask = this.tasks[taskIndex];
+
+    this.tasks.splice(taskIndex, 1);
+    return deletedTask;
+  }
+
+  updateTaskStatus(id: string, status: TaskStatus): Task | undefined {
+    const task = this.getTaskById(id);
+    if (task) task.status = status;
+    return task;
+  }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
+
     const task: Task = {
       id: uuid(),
       title,
